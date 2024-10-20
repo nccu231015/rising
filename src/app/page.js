0f9ebd2f07005f913294de9a48b8d7e5790e9ac1
nccu231015 <istudiodesign.tw@gmail.com>
2024-10-20 22:19:04 +0800

    fixing CV position and cursor:

--- src/app/page.js
+++ src/app/page.js
@@ -246,6 +246,7 @@ export default function Home() {
 
       <ShaderGradientCanvas
         importedfiber={{ ...fiber, ...drei, ...reactSpring }}
+        enableZoom={false}
         style={{
           position: "absolute",
           top: 0,
@@ -255,7 +256,7 @@ export default function Home() {
       >
         <ShaderGradient
           control="query"
-          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=16&color1=%23006979&color2=%23ff810a&color3=%230c0200&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&type=sphere&uAmplitude=2.2&uDensity=0.5&uFrequency=5.5&uSpeed=0.4&uStrength=0.3&uTime=0&wireframe=false&zoomOut=false"
+          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=16&color1=%23006979&color2=%23ff810a&color3=%230c0200&destination=onCanvas&embedMode=on&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&type=sphere&uAmplitude=2.2&uDensity=0.5&uFrequency=5.5&uSpeed=0.4&uStrength=0.3&uTime=0&wireframe=false&zoomOut=false"
         />
       </ShaderGradientCanvas>
 
